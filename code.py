b3641fa552a2a09c8a96f714844fac93db3d182a
adi-bal <adi.ballaki@gmail.com>
2022-02-25 02:57:30 +0800

    finished contributers

--- code.py
+++ code.py
@@ -2,8 +2,8 @@ class contributer:
     def __init__(self, name, skills):
         self.name = name
         self.skills = skills #dictionary that holds skill - levels
-    def show():
-        print("{} {}".format(name ))
+    def show(self):
+        print("{} {}".format(self.name, self.skills ))
 
 class project:
     def __init__(self, name, days, score, bbefore, nroles, skills):
@@ -13,14 +13,23 @@ class project:
         self.bbefore = bbefore
         self.nroles = nroles
         self.skills = skills
-contributers = list()
-projects = list() 
+contributer_list = list()
+projects_list = list() 
 
 f = open("a_an_example.in.txt", "r")
 input = f.readlines()
 f.close()
-contributers = input[0][0]
-projects = input[0][1]
+new_input = list()
+for line in input:
+    line = line.split(" ")
+    new_input.append(line)
+print(new_input)
+
+input = new_input
+
+
+contributers = int(input[0][0])
+projects = int(input[0][1])
 i =1
 while(contributers):
     name = input[i][0]
@@ -31,9 +40,9 @@ while(contributers):
         skills[input[i][0]] = input[i][1]
         i +=1
     new_contributer = contributer(name, skills)
-    contributers.append(new_contributer)
+    contributer_list.append(new_contributer)
     contributers -=1
-        
+"""        
 while(projects):
     name = input[i][0]
     i += 1
@@ -43,14 +52,15 @@ while(projects):
     i += 1
     bbefore = input[i][0]
     i += 1
-    nroles = input[i][0]
+    nroles = int(input[i][0])
     i += 1
     skills = dict()
     for _ in range(nroles):
         skills[input[i][0]] = input[i][1]
         i +=1
-    new_project = project(name, days, score, bbefore, nroles, skills)    
+    new_project = project(name, days, score, bbefore, nroles, skills)
+    projects_list.append(new_project)    
+"""
 
-
-for c in contributer:
+for c in contributer_list:
     c.show()
