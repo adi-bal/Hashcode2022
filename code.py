14f48a71757ef2aedbb8d2cc0aa36f3c0ed63ba4
adi-bal <adi.ballaki@gmail.com>
2022-02-25 02:43:34 +0800

    created input path

--- code.py
+++ code.py
@@ -1,15 +1,56 @@
-class contributers:
+class contributer:
     def __init__(self, name, skills):
         self.name = name
-        self.skills = skills #list that holds skills
+        self.skills = skills #dictionary that holds skill - levels
+    def show():
+        print("{} {}".format(name ))
 
-class projects:
-    def __init__(self, name, days, score, bbefore, nroles):
+class project:
+    def __init__(self, name, days, score, bbefore, nroles, skills):
         self.name = name
         self.days = days
         self.score = score
         self.bbefore = bbefore
-        self.nroles = nroles 
+        self.nroles = nroles
+        self.skills = skills
+contributers = list()
+projects = list() 
 
 f = open("a_an_example.in.txt", "r")
+input = f.readlines()
+f.close()
+contributers = input[0][0]
+projects = input[0][1]
+i =1
+while(contributers):
+    name = input[i][0]
+    n = int(input[i][1])
+    i+=1
+    skills = dict()
+    for _ in range(n):
+        skills[input[i][0]] = input[i][1]
+        i +=1
+    new_contributer = contributer(name, skills)
+    contributers.append(new_contributer)
+    contributers -=1
+        
+while(projects):
+    name = input[i][0]
+    i += 1
+    days = input[i][0]
+    i += 1
+    score  = input[i][0]
+    i += 1
+    bbefore = input[i][0]
+    i += 1
+    nroles = input[i][0]
+    i += 1
+    skills = dict()
+    for _ in range(nroles):
+        skills[input[i][0]] = input[i][1]
+        i +=1
+    new_project = project(name, days, score, bbefore, nroles, skills)    
 
+
+for c in contributer:
+    c.show()
