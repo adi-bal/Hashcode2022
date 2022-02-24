c861323d662636cdcaf3f210299469d1f2eefbdd
adi-bal <adi.ballaki@gmail.com>
2022-02-25 02:12:07 +0800

    first commit

---
+++ code.py
@@ -0,0 +1,15 @@
+class contributers:
+    def __init__(self, name, skills):
+        self.name = name
+        self.skills = skills #list that holds skills
+
+class projects:
+    def __init__(self, name, days, score, bbefore, nroles):
+        self.name = name
+        self.days = days
+        self.score = score
+        self.bbefore = bbefore
+        self.nroles = nroles 
+
+f = open("a_an_example.in.txt", "r")
+
